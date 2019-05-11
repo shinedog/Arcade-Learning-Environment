e62ab06cd18c3538fe89ce5e2d465e7715803570
shinedog <none>
2019-05-11 19:37:58 +0000

    updated shell.nix for pure nix shell to work on environment

--- shell.nix
+++ shell.nix
@@ -3,5 +3,5 @@
 with pkgs;
 
 mkShell {
-  buildInputs = [ git libsdl1.2-dev libsdl-gfx1.2-dev libsdl-image1.2-dev cmake ]
+  buildInputs = [ git SDL2 SDL SDL_gfx SDL_image SDL2_gfx SDL2_image zlib gdb cmake ];
 }
